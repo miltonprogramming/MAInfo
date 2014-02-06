12353f27a5444b9b48812861cf060c2da152b82d
neilchandra <ncsnowboarder@gmail.com>
2014-02-06 13:46:03 -0500

    raj's homescreen stuff

--- src/edu/milton/mainfo/HomeScreen.java
+++ src/edu/milton/mainfo/HomeScreen.java
@@ -11,50 +11,57 @@ public class HomeScreen extends Activity {
 	ImageButton flik;
 	ImageButton saa;
 	ImageButton settings;
+	String email;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_screen);
-		flik=(ImageButton) findViewById(R.id.flik_button);
-        saa=(ImageButton) findViewById(R.id.saa_button);
-        settings=(ImageButton) findViewById(R.id.settings_button);
-        
-        flik.setOnClickListener(new View.OnClickListener() {
-			
+		flik = (ImageButton) findViewById(R.id.flik_button);
+		saa = (ImageButton) findViewById(R.id.saa_button);
+		settings = (ImageButton) findViewById(R.id.settings_button);
+		Intent i = getIntent();
+		if (i.getExtras() != null) {
+			email = i.getStringExtra("email");
+		}
+
+		flik.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), FlikMenuActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						FlikMenuActivity.class);
+				i.putExtra("email", email);
 				startActivity(i);
-				
+
 			}
 		});
-        
-        saa.setOnClickListener(new View.OnClickListener() {
-			
+
+		saa.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), SAAScheduleActivity.class);
+				Intent i = new Intent(getApplicationContext(),
+						SAAScheduleActivity.class);
 				startActivity(i);
-				
+
 			}
 		});
-        //TODO: this is a reference to settings class but we don't have a settings activity?
-        settings.setOnClickListener(new View.OnClickListener() {
-			
+		// TODO: this is a reference to settings class but we don't have a
+		// settings activity?
+		settings.setOnClickListener(new View.OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				Intent i = new Intent(getApplicationContext(), Settings.class);
 				startActivity(i);
-				
+
 			}
 		});
-        
-        
-        
+
 	}
 
 	@Override
